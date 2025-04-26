e6f3fb383103258cecf4b9dcd04f756dffc78e0f
Renan Reis <renanmacfernandes@hotmail.com>
2025-04-26 19:18:08 -0300

    fix entitie

--- clientesApp/src/main/java/com/renan/clientApp/entities/Client.java
+++ clientesApp/src/main/java/com/renan/clientApp/entities/Client.java
@@ -1,27 +1,33 @@
 package com.renan.clientApp.entities;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Table(name = "Cliente")
 @jakarta.persistence.Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class Entity {
+public class Client {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
+	private Long id;
 	private String nome;
 	private Long telefone;
 	private String endereco;
 	private String cpf;
-	private String dataCadastro;
+	@Column(name = "data_cadastro")
+	private LocalDateTime dataCadastro;
 
 }
