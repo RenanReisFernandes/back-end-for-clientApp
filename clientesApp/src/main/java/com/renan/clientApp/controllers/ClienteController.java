d34fcceea3d4068010ce3803af5bb0945dbb39cc
Renan Reis <renanmacfernandes@hotmail.com>
2025-04-26 23:03:36 -0300

    estructure creation

---
+++ clientesApp/src/main/java/com/renan/clientApp/controllers/ClienteController.java
@@ -0,0 +1,10 @@
+package com.renan.clientApp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ClienteController {
+
+}
---
+++ clientesApp/src/main/java/com/renan/clientApp/repositories/ClienteRepositorie.java
@@ -0,0 +1,8 @@
+package com.renan.clientApp.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepositorie {
+
+}
